c81d1b8745811e913ead4e6e5050516b9977a3cc
EgeSythn <eseyith1@gmail.com>
2023-04-11 15:55:34 -0400

    Graph creation

--- app.py
+++ app.py
@@ -1,42 +1,97 @@
 import base64
 import io
 import pandas as pd
+import plotly.graph_objects as go
+import dash
 from dash import Dash, html, dcc, dash_table, no_update
 from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
 import dash_bootstrap_components as dbc
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-
+app = Dash(
+    __name__,
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
+    suppress_callback_exceptions=True,
+)
 
 graph_tab = dbc.Card(
     dbc.CardBody(
-        html.Div(
-            [
-                html.Ul(
-                    [
-                        html.Li(
-                            "You can add data by clicking on the graph, and adjust the parameters!"
-                        ),
-                    ],
-                    style={"fontSize": 15},
+        [
+            html.Div(
+                html.Center(
+                    html.P(
+                        "You can add data by clicking on the graph and adjust the parameters",
+                        style={"frontSize": "15"},
+                    )
                 ),
-            ]
-        )
+            ),
+            dbc.Row(
+                [
+                    dbc.Col(
+                        [
+                            dbc.InputGroup(
+                                [
+                                    dbc.InputGroupText("Feature Value"),
+                                    dbc.Input(id="feature-input", type="text"),
+                                ],
+                                className="mb-3",
+                            ),
+                            dbc.InputGroup(
+                                [
+                                    dbc.InputGroupText("Predicted Value"),
+                                    dbc.Input(id="predicted-input", type="text"),
+                                ],
+                                className="mb-3",
+                            ),
+                            dbc.Button(
+                                "Create Graph",
+                                id="create-graph",
+                                color="primary",
+                                className="mb-3",
+                            ),
+                            html.Div(
+                                [
+                                    dbc.Toast(
+                                        "Please fill in the input boxes",
+                                        id="input-error-toast",
+                                        header="Missing Input",
+                                        is_open=False,
+                                        dismissable=True,
+                                        icon="danger",
+                                        duration=4000,
+                                    )
+                                ],
+                                style={
+                                    "position": "fixed",
+                                    "top": "2rem",
+                                    "right": "2rem",
+                                },
+                            ),
+                        ],
+                        width="auto",
+                        className="mx-auto",
+                    )
+                ]
+            ),
+            dcc.Graph(id="regression-graph", style={"display": "none"}),
+            html.Div(id="graph-visible", style={"display": "none"}, children="False"),
+        ]
     ),
     className="mt-3",
 )
+
 
 file_tab = dbc.Card(
     dbc.CardBody(
         [
             html.Div(
                 [
-                    html.Ul(
+                    html.Center(
                         [
-                            html.Li(
+                            html.P(
                                 "Make sure that the table has appropriate column headers that can be extracted as features"
                             ),
-                            html.Li(
+                            html.P(
                                 [
                                     "Try and make sure that there is no missing data in the table, click ",
                                     html.A(
@@ -135,7 +190,7 @@ app.layout = html.Div(
             ],
             style={"position": "fixed", "top": "2rem", "right": "2rem"},
         ),
-    ]
+    ],
 )
 
 
@@ -158,6 +213,90 @@ def switch_tab(at):
     elif at == "tab-2":
         return file_tab
     return html.P("Error rendering tabs...")
+
+
+@app.callback(
+    [
+        Output("regression-graph", "figure"),
+        Output("input-error-toast", "is_open"),
+        Output("graph-visible", "children"),
+        Output("regression-graph", "style"),
+    ],
+    [
+        Input("create-graph", "n_clicks"),
+        Input("regression-graph", "clickData"),
+    ],
+    [
+        State("feature-input", "value"),
+        State("predicted-input", "value"),
+        State("regression-graph", "figure"),
+    ],
+)
+def update_graph(n_clicks, click_data, feature_value, predicted_value, figure):
+    ctx = dash.callback_context
+    if (
+        not ctx.triggered
+        or ctx.triggered[0]["prop_id"].split(".")[0] == "regression-graph"
+    ):
+        raise PreventUpdate
+
+    if feature_value is None or predicted_value is None:
+        return no_update, True, no_update, no_update
+
+    if (
+        n_clicks is not None
+        and feature_value is not None
+        and predicted_value is not None
+    ):
+        graph_visible = True
+
+    if click_data is None and graph_visible is False:
+        return no_update, no_update, no_update, {"display": "none"}
+
+    if click_data is None:
+        return (
+            go.Figure(
+                layout=go.Layout(
+                    xaxis_title=feature_value,
+                    yaxis_title=predicted_value,
+                    hovermode="closest",
+                    clickmode="event+select",
+                )
+            ),
+            False,
+            True,
+            {"display": "block"},
+        )
+
+    if figure is None or figure["data"] == []:
+        raise PreventUpdate
+
+    x = click_data["points"][0]["x"]
+    y = click_data["points"][0]["y"]
+
+    if figure is None:
+        figure = go.Figure()
+
+    if (
+        ctx.triggered[0]["prop_id"] == "regression-graph.clickData"
+    ):  # Add this condition
+        new_trace = go.Scatter(
+            x=[x],
+            y=[y],
+            mode="markers",
+            marker=dict(color="red", symbol="x", size=10),
+            name=f"({x:.2f}, {y:2f})",
+        )
+
+        figure.add_trace(new_trace)
+
+    figure.update_layout(
+        xaxis_title=feature_value,
+        yaxis_title=predicted_value,
+        hovermode="closest",
+    )
+
+    return figure, False, True, {"display": "block"}
 
 
 @app.callback(
